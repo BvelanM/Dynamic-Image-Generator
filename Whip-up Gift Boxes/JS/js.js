0bf6a7843a943bdb0a95dbeeb41ec09326bb5281
Bala Velan <107044031+BvelanM@users.noreply.github.com>
2022-11-07 09:12:12 +0530

    Add files via upload

---
+++ Whip-up Gift Boxes/JS/js.js
@@ -0,0 +1,9 @@
+function fun(){
+var n=prompt("The Count Sweet Child O' Mine ")
+var im=document.querySelector('#goddamn');
+for (i=0;i<n;i++){
+var eh=document.createElement('div');
+eh.innerHTML='<img src="../Media/gift.gif">';
+im.appendChild(eh);
+}
+}
